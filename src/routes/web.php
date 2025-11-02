02ef7c8a0acc130b5ba57c57b36ce012db96f13e
megumi <megumi.chocolate1209@gmail.com>
2025-11-03 03:38:17 +0900

    ProductController追加、ルーティング修正、docker-compose更新、ER図とassets追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 商品一覧
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+
+// 商品追加ページ表示
+Route::get('/products/register', [ProductController::class, 'create'])->name('products.create');
+
+// 商品詳細
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+// 商品更新
+Route::match(['put', 'post'], '/products/{product}/update', [ProductController::class, 'update'])->name('products.update');
+
+// 商品保存処理
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+// 商品検索
+Route::match(['get', 'post'], '/products/search', [ProductController::class, 'search'])->name('products.search');
+
+// 商品削除
+Route::match(['delete', 'post'], '/products/{product}/delete', [ProductController::class, 'delete'])->name('products.delete');
